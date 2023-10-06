bcd8d45f8b93e441fc500d559b3ebffd15f6daab
terenceDev <terence.arseneault@hotmail.com>
2023-10-06 11:26:35 -0300

    Refactored the nav and routes with newer createBrowserRouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,13 @@
 import NavBar from "./Components/NavBar/NavBar";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+//pages
 import Home from "./Components/Home/Home";
 import Menu from "./Components/Menu/Menu";
 import Events from "./Components/Events/Events";
@@ -8,54 +15,35 @@ import AboutUs from "./Components/AboutUs/AboutUs";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 import SignIn from "./Components/SignIn/SignIn";
+import EventDetails from "./Components/Events/EventDetails";
+
+//Layouts
+import RootLayout from "./Components/Layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} exact />
+      <Route path="menu" element={<Menu />} />
+      <Route path="events" element={<Events />} />
+      <Route path="about" element={<AboutUs />} />
+      <Route path="signIn" element={<SignIn />} />
+      <Route path="contact" element={<Contact />} />
+      {/* <Route path="eventDetails" element={<EventDetails />} /> */}
+    </Route>
+  )
+);
 
 function App() {
   return (
-    <main>
+    <main-wrapper>
       <div className="wrapper">
-        <Router>
-          <div>
-            <NavBar />
-            <div className="content">
-              <Routes>
-                <Route
-                  path="/"
-                  element={<Home />}
-                  exact
-                  activeClassName="active"
-                />
-                <Route
-                  path="/menu"
-                  element={<Menu />}
-                  activeClassName="active"
-                />
-                <Route
-                  path="/events"
-                  element={<Events />}
-                  activeClassName="active"
-                />
-                <Route
-                  path="/about"
-                  element={<AboutUs />}
-                  activeClassName="active"
-                />
-                <Route
-                  path="/signIn"
-                  element={<SignIn />}
-                  activeClassName="active"
-                />
-                <Route
-                  path="/contact"
-                  element={<Contact />}
-                  activeClassName="active"
-                />
-              </Routes>
-            </div>
-          </div>
-        </Router>
+        <div className="content">
+          <RouterProvider router={router} />
+        </div>
       </div>
       <Footer />
-    </main>
+    </main-wrapper>
   );
 }
 
--- client/src/Components/Events/EventDetails.js
+++ client/src/Components/Events/EventDetails.js
@@ -1,13 +1,13 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 const EventDetails = (props) => {
-  const { eventId } = useParams();
+  // const { eventId } = useParams();
 
   // Fetch event details using the 'eventId' from the route parameter
 
   return (
     <div>
       <h2>Event Details</h2>
-      <p>Event ID: {eventId}</p>
+      {/* <p>Event ID: {eventId}</p> */}
       {/* Display other event details based on the fetched data */}
     </div>
   );
