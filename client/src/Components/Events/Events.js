9ba8c67f51ed08c64c9a9a7e2f1dfc6b094034da
terenceDev <terence.arseneault@hotmail.com>
2023-10-06 07:25:54 -0300

    added link buttons for events and also added cart in navbar

--- client/src/Components/Events/Events.js
+++ client/src/Components/Events/Events.js
@@ -7,21 +7,27 @@ import EventsCard from "./EventsCard";
 const Events = () => {
   const eventListings = [
     {
+      eventId: 1,
       image: eventOne,
       title: "Bug Bash & Bites",
       description:
         "Debug, munch, and network with techies who love solving problems.",
+      date: "November 3rd",
     },
     {
+      eventId: 2,
       image: eventTwo,
       title: "API & Appetizers Mixer",
       description:
         "Network and explore the world of APIs while enjoying appetizers.",
+      date: "November 6th",
     },
     {
+      eventId: 3,
       image: eventThree,
       title: "Web Dev Happy Hour",
       description: "An informal gathering for web developers to network.",
+      date: "November 10th",
     },
   ];
 
@@ -30,9 +36,11 @@ const Events = () => {
       <div className="eventCards">
         {eventListings.map((item) => (
           <EventsCard
+            eventId={item.eventId}
             eventImage={item.image}
             eventTitle={item.title}
             eventDescription={item.description}
+            date={item.date}
           />
         ))}
       </div>
--- client/src/Components/Menu/Menu.js
+++ client/src/Components/Menu/Menu.js
@@ -17,30 +17,35 @@ const Menu = () => {
       title: "JavaScript Jolt",
       description:
         "A zesty mix of citrus and spirits for the ultimate code refresher.",
+      price: 7,
     },
     {
       id: 2,
       image: highball,
       title: "HTML Highball",
       description: "A classic cocktail with a web development twist.",
+      price: 8,
     },
     {
       id: 3,
       image: cooler,
       title: "CSS Cooler",
       description: "A cool and refreshing drink that styles up your evening.",
+      price: 8,
     },
     {
       id: 4,
       image: pythonPunch,
       title: "Python Punch",
       description: "A punchy blend of fruity flavors for Python enthusiasts.",
+      price: 7,
     },
     {
       id: 5,
       image: rubyMartini,
       title: "Ruby Red Martini",
       description: "An elegant martini with a ruby twist for Ruby lovers.",
+      price: 6,
     },
     {
       id: 6,
@@ -48,18 +53,21 @@ const Menu = () => {
       title: "Java Jive",
       description:
         "A smooth coffee-infused cocktail to keep your coding energy high.",
+      price: 9,
     },
     {
       id: 7,
       image: sour,
       title: "SQL Sour",
       description: "A tangy and sour mix that queries your taste buds.",
+      price: 7,
     },
     {
       id: 8,
       image: collins,
       title: "C++ Collins",
       description: "A classic collins cocktail with a C++ twist. ",
+      price: 10,
     },
   ];
   return (
@@ -73,6 +81,7 @@ const Menu = () => {
               image={item.image}
               title={item.title}
               description={item.description}
+              price={item.price}
             />
           </div>
         ))}
